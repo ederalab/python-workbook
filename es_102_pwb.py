c7f53f7ecd8883fc15d9ace4257219ce2647958d
Edera <edera.marketinglab@gmail.com>
2021-03-20 12:24:04 +0100

    Esercizi fino al 17 marzo

--- es_102_pwb.py
+++ es_102_pwb.py
@@ -1,4 +1,4 @@
-#check a password
+# check a password
 def checkPassword(password):
     check_lower = False
     check_upper = False
@@ -11,20 +11,21 @@ def checkPassword(password):
             check_upper = True
         if c >= "0" and c <= "9":
             check_digit = True
-            
+
     if len(password) >= 8 and check_lower == True and check_upper == True and check_digit == True:
         return True
     else:
         return False
-            
+
 
 def main():
     password = input("Enter the password: ")
     check_pass = checkPassword(password)
     if check_pass == True:
-        print ("The password is valid")
+        print("The password is valid")
     else:
-        print ("The password is not valid")
-    
+        print("The password is not valid")
+
+
 if __name__ == "__main__":
     main()
